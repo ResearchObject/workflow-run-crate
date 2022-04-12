2185b6cfc6f92e1e1924b3633170e7e814a2fc8a
simleo <simleo@crs4.it>
2022-04-12 12:01:20 +0200

    parse provenance and add action

--- tools/cwlprov_to_crate/cwlprov_to_crate.py
+++ tools/cwlprov_to_crate/cwlprov_to_crate.py
@@ -22,6 +22,126 @@ from pathlib import Path
 
 # from cwl_utils.parser import load_document_by_uri
 from rocrate.rocrate import ROCrate
+from rocrate.model.contextentity import ContextEntity
+
+
+class Thing:
+
+    def __init__(self, id_, label=None):
+        self.id_ = id_
+        self.label = label
+
+
+class Agent(Thing):
+    pass
+
+
+class SoftwareAgent(Agent):
+
+    def __init__(self, id_, label=None, image=None):
+        super().__init__(id_, label=label)
+        self.image = image
+
+
+class WorkflowEngine(SoftwareAgent):
+    pass
+
+
+class Person(Agent):
+
+    def __init__(self, id_, label=None, name=None):
+        super().__init__(id_, label=label)
+        self.name = name or label
+
+
+class Activity(Thing):
+
+    def __init__(self, id_, label=None):
+        super().__init__(id_, label=label)
+        self.starter = None
+        self.ender = None
+        self.start_time = None
+        self.end_time = None
+
+
+class ProcessRun(Activity):
+    pass
+
+
+class WorkflowRun(Activity):
+
+    def __init__(self, id_, label=None):
+        super().__init__(id_, label=label)
+        self.steps = []
+
+
+class Provenance:
+
+    def __init__(self, path_or_file):
+        try:
+            self.data = json.load(path_or_file)
+        except AttributeError:
+            with open(path_or_file) as f:
+                self.data = json.load(f)
+        self.agents = self.__read_agents()
+        self.activities = self.__read_activities()
+        self.__read_start_end()
+
+    @staticmethod
+    def get_types(record):
+        entry = record.get("prov:type", [])
+        if not isinstance(entry, list):
+            entry = [entry]
+        try:
+            return [_["$"] for _ in entry]
+        except (TypeError, KeyError):
+            raise ValueError(f"Unexpected type entry: {entry!r}")
+
+    def __read_agents(self):
+        agents = {}
+        for id_, record in self.data["agent"].items():
+            types = set(Provenance.get_types(record))
+            if "prov:Person" in types:
+                agents[id_] = Person(
+                    id_,
+                    label=record.get("prov:label"),
+                    name=record.get("prov:name"),
+                )
+            elif "wfprov:WorkflowEngine" in types:
+                agents[id_] = WorkflowEngine(id_, label=record.get("prov:label"))
+            elif "prov:SoftwareAgent" in types:
+                agents[id_] = SoftwareAgent(
+                    id_,
+                    label=record.get("prov:label"),
+                    image=record.get("cwlprov:image"),
+                )
+            else:
+                agents[id_] = Agent(id_, label=record.get("prov:label"))
+        return agents
+
+    def __read_activities(self):
+        activities = {}
+        for id_, record in self.data["activity"].items():
+            types = set(Provenance.get_types(record))
+            if "wfprov:WorkflowRun" in types:
+                activities[id_] = WorkflowRun(id_, label=record.get("prov:label"))
+            elif "wfprov:ProcessRun" in types:
+                activities[id_] = ProcessRun(id_, label=record.get("prov:label"))
+            else:
+                activities[id_] = Activity(id_, label=record.get("prov:label"))
+        return activities
+
+    def __read_start_end(self):
+        for dummy, record in self.data["wasStartedBy"].items():
+            activity = self.activities.get(record.get("prov:activity"))
+            if activity:
+                activity.starter = self.agents.get(record.get("prov:starter"))
+                activity.start_time = record.get("prov:time")
+        for dummy, record in self.data["wasEndedBy"].items():
+            activity = self.activities.get(record.get("prov:activity"))
+            if activity:
+                activity.ender = self.agents.get(record.get("prov:ender"))
+                activity.end_time = record.get("prov:time")
 
 
 def get_workflow(root_dir):
@@ -37,6 +157,27 @@ def get_workflow(root_dir):
     return wf_path, input_data, output_data
 
 
+def read_prov(root_dir):
+    prov_dir = root_dir / "metadata" / "provenance"
+    with open(prov_dir / "primary.cwlprov.json") as f:
+        return json.load(f)
+
+
+def add_action(crate, prov):
+    sel = [_ for _ in prov.activities.values() if type(_) == WorkflowRun]
+    if len(sel) != 1:
+        raise ValueError(f"Unexpected number of workflow runs: {len(sel)}")
+    wf_run = sel[0]
+    action = crate.add(ContextEntity(crate, properties={
+        "@type": "CreateAction",
+        "name": wf_run.label,
+        "startTime": wf_run.start_time,
+        "endTime": wf_run.end_time,
+    }))
+    action["instrument"] = crate.mainEntity
+    return action
+
+
 def make_crate(args):
     crate = ROCrate(gen_preview=False)
     wf_source, input_data, output_data = get_workflow(args.root)
@@ -46,9 +187,13 @@ def make_crate(args):
         wf_source, wf_source.name, main=True, lang="cwl",
         lang_version=wf_data["cwlVersion"], gen_cwl=False
     )
-    # workflow["name"] = ? How to map to the original wf file in snapshot?
+    # How to map to the original workflow file in "snapshot"?
+    workflow["name"] = args.workflow_name or args.root.name
     if args.license:
         crate.root_dataset["license"] = args.license
+    prov = Provenance(args.root / "metadata" / "provenance" / "primary.cwlprov.json")
+    action = add_action(crate, prov)
+    crate.root_dataset["mentions"] = [action]
     if args.output.endswith(".zip"):
         crate.write_zip(args.output)
     else:
@@ -73,4 +218,6 @@ if __name__ == "__main__":
                         help="output RO-Crate directory or zip file")
     parser.add_argument("-l", "--license", metavar="STRING",
                         help="license URL (or WorkflowHub-accepted id)")
+    parser.add_argument("-w", "--workflow-name", metavar="STRING",
+                        help="original workflow name")
     main(parser.parse_args())
