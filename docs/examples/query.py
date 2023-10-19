1a606a8f5ff0d985f2ef49221a458134da485ee5
simleo <simleo@crs4.it>
2023-10-19 14:40:38 +0200

    add script to perform an example SPARQL query

---
+++ docs/examples/query.py
@@ -0,0 +1,52 @@
+# Copyright 2023 CRS4.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""\
+Perform a sample query on a Workflow Run RO-Crate.
+
+The query returns all actions together with their instruments.
+"""
+
+from pathlib import Path
+import argparse
+import rdflib
+
+
+query = """\
+PREFIX schema: <http://schema.org/>
+SELECT ?action ?instrument
+WHERE {
+  ?action a schema:CreateAction .
+  ?action schema:instrument ?instrument
+}
+"""
+
+
+def main(args):
+    args.crate = Path(args.crate)
+    g = rdflib.Graph()
+    g.parse(args.crate / "ro-crate-metadata.json")
+    qres = g.query(query)
+    for row in qres:
+        print(f"action: {row.action}")
+        print(f"instrument: {row.instrument}")
+        print()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument("crate", metavar="RO-Crate", help="RO-Crate directory")
+    main(parser.parse_args())
